1d65f5026b0fd2d645081706a2d5bba260310870
Declan Nnadozie <zedonbiz@gmail.com>
2025-03-12 20:53:23 +0100

    update

--- src/ckton_dashboard/src/config.js
+++ src/ckton_dashboard/src/config.js
@@ -3,4 +3,5 @@ export const CKTON_DASHBOARD_CANISTER_ID = process.env.CANISTER_ID_CKTON_DASHBOA
 export const II_URL = process.env.NODE_ENV === 'development' ? " http://be2us-64aaa-aaaaa-qaabq-cai.localhost:4943" : 'https://identity.ic0.app';
 // 'mainnet' or 'testnet'
 export const IC_HOST = process.env.NODE_ENV === 'development' ? "http://127.0.0.1:4943" : 'https://icp0.io';
-export const CKTON_LEDGER_CANISTER_ID = process.env.CANISTER_ID_CKTON_LEDGER; //process.env.NODE_ENV === 'development' ? 'c2lt4-zmaaa-aaaaa-qaaiq-cai' : 'd3sjl-xaaaa-aaaam-aegnq-cai';
+export const CKTON_LEDGER_CANISTER_ID = process.env.CANISTER_ID_ICRC1_LEDGER_CANISTER; //process.env.NODE_ENV === 'development' ? 'c2lt4-zmaaa-aaaaa-qaaiq-cai' : 'd3sjl-xaaaa-aaaam-aegnq-cai';
+export const CKTON_INDEXER_CANISTER_ID = process.env.CANISTER_ID_ICRC1_INDEX_CANISTER; //process.env.NODE_ENV === 'development' ? 'c2lt4-zmaaa-aaaaa-qaaiq-cai' : 'd3sjl-xaaaa-aaaam-aegnq-cai';
