bba57668e6fe3f1333a3edb84d6a58284426d313
Declan Nnadozie <zedonbiz@gmail.com>
2025-03-12 21:21:04 +0100

    update

--- src/ckton_dashboard/src/App.vue
+++ src/ckton_dashboard/src/App.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue';
 import { ckton_dashboard } from 'declarations/ckton_dashboard/index';
 import { createActor as createCktonMinter } from 'declarations/ckton_minter/index';
-import { CKTON_DASHBOARD_CANISTER_ID, CKTON_LEDGER_CANISTER_ID, CKTON_MINTER_CANISTER_ID, IC_HOST, II_URL } from './config';
+import { CKTON_DASHBOARD_CANISTER_ID, CKTON_LEDGER_CANISTER_ID, CKTON_MINTER_CANISTER_ID, CKTON_INDEXER_CANISTER_ID, IC_HOST, II_URL } from './config';
 import { AuthClient } from '@dfinity/auth-client';
 import { decodeIcrcAccount, IcrcLedgerCanister } from "@dfinity/ledger-icrc"
 import { Principal } from '@dfinity/principal';
@@ -590,6 +590,28 @@ async function copyToClipboard(text) {
                 </span>
                 <span v-else class="break-all">{{ CKTON_MINTER_CANISTER_ID }}</span>
                 <button @click="copyToClipboard(CKTON_MINTER_CANISTER_ID)" 
+                        class="ml-2 text-gray-500 hover:text-gray-700 focus:outline-none" 
+                        title="Copy to clipboard">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
+                    <path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            
+            <div class="flex flex-col sm:flex-row sm:items-center p-3 bg-gray-50 rounded">
+              <span class="font-medium mr-2 mb-2 sm:mb-0">Indexer Canister:</span>
+              <div class="flex-1 flex items-center">
+                <span v-if="isProduction" class="break-all">
+                  <a :href="`https://dashboard.internetcomputer.org/canister/${CKTON_INDEXER_CANISTER_ID}`" 
+                     target="_blank" 
+                     class="text-blue-600 hover:underline">
+                    {{ CKTON_INDEXER_CANISTER_ID }}
+                  </a>
+                </span>
+                <span v-else class="break-all">{{ CKTON_INDEXER_CANISTER_ID }}</span>
+                <button @click="copyToClipboard(CKTON_INDEXER_CANISTER_ID)" 
                         class="ml-2 text-gray-500 hover:text-gray-700 focus:outline-none" 
                         title="Copy to clipboard">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
